91c0f10ac2c83935e24fa6881698ea8cc934cff5
elisabeth <elisabeth@shoogee.com>
2026-02-20 16:50:25 +0100

    updates from work laptop

--- nvim/lua/plugins/colorschemes.lua
+++
@@ -1,5 +0,0 @@
-return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
-    { "rmehri01/onenord.nvim", branch = "main"}
-}
--- nvim/lua/plugins/snacks.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"folke/snacks.nvim",
-	---@type snacks.Config
-	opts = {
-		indent = { enabled = true },
-		statuscolumn = { enabled = true }
-  }
-}
